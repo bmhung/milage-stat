b2f594c1a7ac82aed2f8846320ba772e98d046e5
bmhung <bui.m.hung@gmail.com>
2025-04-19 18:47:13 +0700

    Add DaisyUI

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -1,6 +1,22 @@
-<form>
-	<div class="box-border border-x-emerald-50"></div>
+<form class="grid grid-cols-[100px_1fr] items-center">
 	<label for="odo">ODO</label>
 	<input id="odo" type="text" />
-	<p></p>
+
+	<label for="date">Date</label>
+	<input id="date" type="date" />
+
+	<label for="price">Price</label>
+	<input id="price" type="text" />
+
+	<label for="amount">Amount</label>
+	<input id="amount" type="text" />
+
+	<label for="total">Total</label>
+	<input id="total" type="text" />
+
+	<div class="col-span-2">
+		<button class="btn btn-primary w-full" type="submit" value="submit" aria-label="submit">
+			Submit
+		</button>
+	</div>
 </form>
